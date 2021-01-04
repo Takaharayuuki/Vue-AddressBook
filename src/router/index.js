275438d8728d2d93ad7aea12eaf1fd26c6d66113
Takaharayuuki <takahara.yuuki22@gmail.com>
2021-01-04 14:05:15 +0900

    feat: 連絡先追加画面の実装

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Addresses from '../views/Addresses'
+import AddressForm from '../components/AddressForm'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/addresses',
     name: 'Addresses',
     component: Addresses
+  },
+  {
+    path: '/addresses/:address_id?/edit',
+    name: 'Address_edit',
+    component: AddressForm
   },
   {
     path: '/about',
